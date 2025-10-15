b7e66347468dfba1378002fadf9ad9e8f02a0990
Avokade <melvin.gieswein@gmail.com>
2025-10-15 14:02:42 +0200

    wow i push with vscode

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/react-portfolio/', // 👈 Wichtig!
   plugins: [
     react({
       babel: {
