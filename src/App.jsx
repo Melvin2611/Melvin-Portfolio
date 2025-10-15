cf62df88957034b1b242897ee14227238c443d1e
Avokade <melvin.gieswein@gmail.com>
2025-10-15 13:54:15 +0200

    Double Router in App.jsx fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -10,7 +10,7 @@ import "./index.css";
 
 function App() {
   return (
-    <Router>
+    <>
       <Header />
       <main>
         <Routes>
@@ -22,7 +22,7 @@ function App() {
         </Routes>
       </main>
       <Footer />
-    </Router>
+    </>
   );
 }
 
