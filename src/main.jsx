f92c5ef51cc15ed70ad04db4e74dadd9433ee7f0
Avokade <melvin.gieswein@gmail.com>
2025-10-22 08:16:05 +0200

    name change

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css"
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/react-portfolio">
+    <BrowserRouter basename="/Melvin-portfolio">
       <App />
     </BrowserRouter>
   </React.StrictMode>
